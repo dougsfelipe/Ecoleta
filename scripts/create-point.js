b6c46cf66d4c0444c28e010fbc29c7b822d4eac1
dougsfelipe <dfcs@cin.ufpe.br>
2020-06-03 21:29:50 -0300

    Pagina de coleta feita

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -25,17 +25,60 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufvalue}/municipios`;
 
+    ufcities.innerHTML= "<option value> Selecione a Cidade </option>";
+    ufcities.disabled = true;
+
     fetch(url).then( res =>  res.json() )
     .then( cities => {
         
+
         for(const city of cities){
-            ufcities.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            ufcities.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         ufcities.disabled = false;
     })
 }
 
-
 document.querySelector("select[name=uf]")
 .addEventListener("change",getCities)
+
+// itens de coleta
+
+const itensdeColeta = document.querySelectorAll(".itens-grid li");
+
+for(const item of itensdeColeta){
+    item.addEventListener("click",handleSelectedItem);
+}
+
+const collecteditens = document.querySelector("input[name=itens]")
+
+let selectedItens = [];
+
+function handleSelectedItem(event){
+
+    const itemLi = event.target;
+
+    itemLi.classList.toggle("selected");
+
+    const itemID = event.target.dataset.id;
+
+    const alredySelected = selectedItens.findIndex( item => item == itemID)
+
+    if(alredySelected >= 0){
+        const filterItems = selectedItens.filter( item => {
+            const ItemIsDifferent = item != itemID;
+            return ItemIsDifferent;
+        } )
+
+        selectedItens = filterItems;
+    }else{
+        selectedItens.push(itemID);
+    }
+
+    collecteditens.value = selectedItens;
+
+ 
+
+    console.log(event.target.dataset.id)
+}
